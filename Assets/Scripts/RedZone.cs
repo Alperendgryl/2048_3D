5de0ebe27bd324e4cb4304911c7dd819be732609
Alperendgryl <alperen-2000-@hotmail.com>
2022-07-12 20:16:38 +0300

    12.07.22

---
+++ Assets/Scripts/RedZone.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+public class RedZone : MonoBehaviour
+{
+    public static bool gameEnded = false;
+    private void OnTriggerStay(Collider other)
+    {
+        Cube cube = other.GetComponent<Cube>();
+
+        if (cube != null)
+        {
+            if (!cube.isMainCube && cube.cubeRigidbody.velocity.magnitude < .1f)
+            {
+                gameEnded = true;
+            }
+        }
+    }
+}
